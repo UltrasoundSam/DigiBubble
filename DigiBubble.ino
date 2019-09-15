dababfb7f77f007d21d8f1d51a069cdd583fbca6
Sam <sam@raspberrypi>
2019-09-15 19:31:52 +0100

    Combined acceleromete and laser ToF measurement into same sketch

--- DigiBubble.ino
+++ DigiBubble.ino
@@ -2,6 +2,7 @@
 
 #include <Wire.h>
 #include <SPI.h>
+#include <VL53L0X.h>
 #include <Adafruit_LIS3DH.h>
 #include <Adafruit_Sensor.h>
 
@@ -9,11 +10,12 @@
 #define LIS3DH_CLK 13
 #define LIS3DH_MISO 12
 #define LIS3DH_MOSI 11
-// Used for hardware & software SPI
 #define LIS3DH_CS 10
 
-// software SPI
+// Create LIS3DH (orientation) Object
 Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS, LIS3DH_MOSI, LIS3DH_MISO, LIS3DH_CLK);
+// Create VL53L0X (distance) object
+VL53L0X IRToF;
 
 // Defining a struct for saving roll and pitch
 struct orient {
@@ -25,7 +27,8 @@ struct orient {
 const float radtoang = 180 / 3.14159265;
 const int numaves = 16;
 struct orient level;
-float total;
+float total_pitch;
+float total_dist;
 
 void setup(void) {
 #ifndef ESP8266
@@ -33,6 +36,7 @@ void setup(void) {
 #endif
 
   Serial.begin(9600);
+  Wire.begin();
   Serial.println("LIS3DH test!");
   
   if (! lis.begin(0x18)) {   // change this to 0x19 for alternative i2c address
@@ -42,11 +46,15 @@ void setup(void) {
   Serial.println("LIS3DH found!");
   
   lis.setRange(LIS3DH_RANGE_2_G);   // 2, 4, 8 or 16 G!
+
+  // Set up Distance meter
+  IRToF.init();
+  IRToF.setTimeout(500);
+  IRToF.setMeasurementTimingBudget(2000);
   
-  Serial.print("Range = "); Serial.print(2 << lis.getRange());  
-  Serial.println("G");
 }
 
+// Function for turning data into pitch and roll values
 struct orient alignments(float accx, float accy, float accz){
   struct orient alignment;
   alignment.roll = asin(accy / accz) * radtoang;
@@ -57,18 +65,22 @@ struct orient alignments(float accx, float accy, float accz){
 void loop() {
   lis.read();      // get X Y and Z data at once
 
-  /* Or....get a new sensor event, normalized */ 
+  /* Get a new sensor event, normalized */ 
   sensors_event_t event; 
   lis.getEvent(&event);
   
-  total = 0;
+  total_pitch = 0;
+  total_dist = 0;
   for (int i = 0; i < numaves; i++){
     level = alignments(event.acceleration.x, event.acceleration.y, event.acceleration.z);
-    total += level.pitch;
+    total_pitch += level.pitch;
+    total_dist += IRToF.readRangeSingleMillimeters();
     delay(50);
   }
-  total /= numaves;
-  Serial.print(" \tRoll: "); Serial.println(total);
+  total_pitch /= numaves;
+  total_dist /= numaves;
+  Serial.print(" \tRoll: "); Serial.print(total_pitch);
+  Serial.print(" \tDist: "); Serial.println(total_dist);
   Serial.println();
  
    
